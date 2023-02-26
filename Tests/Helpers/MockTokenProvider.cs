901c2974d475fc3349d0e837e073c0f3734e112b
tenx29 <83706729+tenx29@users.noreply.github.com>
2023-02-26 19:03:20 +0200

    Add test project

---
+++ Tests/Helpers/MockTokenProvider.cs
@@ -0,0 +1,19 @@
+﻿using Roblox;
+
+namespace Tests.Helpers;
+
+/// <summary>
+///     Provides a mock token provider for testing.
+/// </summary>
+public class MockTokenProvider
+{
+    /// <summary>
+    ///     Mock .ROBLOSECURITY cookie.
+    /// </summary>
+    public static string Cookie = $"|_{new string('0', 712)}";
+
+    /// <summary>
+    ///     Mock RobloxClientCredential.
+    /// </summary>
+    public static RobloxClientCredential Credential = new(Cookie);
+}
